0027cc23cbd799cae4552ca86e1738e66b5f37f7
Karen Lundgren <karen.e.lundgren@gmail.com>
2012-09-08 19:56:05 -0600

    Added flash_class method

--- app/helpers/cooperative_helper.rb
+++ app/helpers/cooperative_helper.rb
@@ -16,4 +16,12 @@ module CooperativeHelper
   def container_title
     @title
   end
+  
+  def flash_class(level)
+    case level
+      when :notice then "alert-info"
+      when :error then "alert-error"
+      when :alert then "alert-warning"
+    end
+  end
 end
