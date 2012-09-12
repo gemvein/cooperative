eee9b2bffe53c712bb6eea2ca42f963d150d54b3
Karen Lundgren <karen.e.lundgren@gmail.com>
2012-09-12 10:00:03 -0600

    Temporarily switched home breadcrumb to string instead of translated symbol because the symbol was broken.

--- app/controllers/cooperative_controller.rb
+++ app/controllers/cooperative_controller.rb
@@ -2,7 +2,7 @@ class CooperativeController < ActionController::Base
   protect_from_forgery
   before_filter :set_locale
   
-  add_breadcrumb :home.l, '/'
+  add_breadcrumb 'Home', '/'
   def set_locale
     I18n.local = params[:lang] if params[:lang].present?
   end
