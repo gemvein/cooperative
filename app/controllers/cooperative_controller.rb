59a50ab7497cc41489930f02b5c9827b21432e5c
Karen Lundgren <karen.e.lundgren@gmail.com>
2012-09-11 22:03:02 -0600

    Added home breadcrumb

--- app/controllers/cooperative_controller.rb
+++ app/controllers/cooperative_controller.rb
@@ -1,6 +1,8 @@
 class CooperativeController < ActionController::Base
   protect_from_forgery
   before_filter :set_locale
+  
+  add_breadcrumb :home.l, '/'
   def set_locale
     I18n.local = params[:lang] if params[:lang].present?
   end
