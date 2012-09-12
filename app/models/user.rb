e0b233ae9117e64bb24c78b1634f78216aab50bf
Karen Lundgren <karen.e.lundgren@gmail.com>
2012-09-12 10:01:11 -0600

    Added some fields to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :nickname, :password, :password_confirmation, :remember_me, :public, :bio
   # attr_accessible :title, :body
+  validates_presence_of :nickname
 end
