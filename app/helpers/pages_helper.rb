f7dfcc5a31ec71ddda2d356ebec79cd60ef523f3
Karen Lundgren <karen.e.lundgren@gmail.com>
2012-09-11 22:04:22 -0600

    Added pages_menu_items

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,8 @@
 module PagesHelper
+  def pages_menu_items
+    @pages = Page.where({:public => true, :parent_id => [nil, 0, ''], :pageable_type => [nil, 0, ''], :pageable_id => [nil, 0, '']})
+    html = ""
+    html << render('pages/menu_items')
+    html.html_safe
+  end
 end
