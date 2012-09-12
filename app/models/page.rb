44617afc4a9b3f791db986cdd11af1d8cc4026a7
Karen Lundgren <karen.e.lundgren@gmail.com>
2012-09-11 22:04:46 -0600

    Added ancestry, path, and find_by_path

--- app/models/page.rb
+++ app/models/page.rb
@@ -11,4 +11,46 @@ class Page < ActiveRecord::Base
   belongs_to :pageable, :polymorphic => true
   attr_accessible :body, :description, :keywords, :public, :title, :parent_id, :pageable_id, :pageable_type
   validates_presence_of :slug, :title, :body
+  
+  def ancestry
+    if parent
+      my_ancestry = parent.ancestry
+      my_ancestry << parent
+    else
+      my_ancestry = []
+    end
+  end
+  
+  def path
+    '/pages/' + path_parts.join('/')
+  end
+  
+  def path_parts
+    parts = []
+    unless ancestry.empty?
+      for ancestor in ancestry
+        parts << ancestor.slug
+      end
+    end
+    parts << slug
+    parts
+  end
+  
+  def self.find_all_by_path(path)
+    parts = path.split('/')
+    conditions = {:parent_id => [0, nil, '']}
+    for part in parts
+      current_pages = self.where(conditions).find_all_by_slug(part)
+      current_page_ids = []
+      for current_page in current_pages
+        current_page_ids << current_page.id
+      end
+      conditions = {:parent_id => current_page_ids}
+    end
+    current_pages
+  end
+  
+  def self.find_by_path(path)
+    self.find_all_by_path(path).first
+  end
 end
