1fe1aa1c05f93a442eabb199fd5a40a5c657901c
Karen Lundgren <karen.e.lundgren@gmail.com>
2012-09-11 22:01:22 -0600

    Updated tests to go with new hierarchical pages

--- spec/requests/layout_spec.rb
+++ spec/requests/layout_spec.rb
@@ -9,8 +9,29 @@ describe "Layout" do
     assert_select '.container', :minimum => 1
   end
   
-  it "should show the configured application name" do
+  it "shows the configured application name" do
     get '/'
     assert_select '.brand', Cooperative.configuration.application_name
+  end
+  
+  it "shows the appropriate page links" do
+    visit cooperative.home_path
+    click_link "About Us"
+    click_link "Privacy Policy"
+    click_link "Terms of Use"
+    click_link "Contact Us"
+    click_link "Home"
+  end
+  
+  it "shows child links on their parent pages only" do
+    parent_page = FactoryGirl.create(:page, :title => 'Parent Page')
+    child_page = FactoryGirl.create(:page, :title => 'Child Page', :parent_id => parent_page.id)
+    visit parent_page.path
+    page.should have_selector '.well li a', :text => 'Child Page'
+    click_link 'Child Page'
+    page.should have_selector '.breadcrumb li a', :text => 'Parent Page'
+    click_link 'Parent Page'
+    click_link 'Home'
+    page.should_not have_selector '.nav li a', :text => 'Child Page'
   end
 end
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -4,8 +4,8 @@ describe "Pages" do
   include_context 'load site'
   it "shows pages" do
     my_page = FactoryGirl.create(:page)
-    visit cooperative.page_path(my_page)
-    current_url.should match /#{my_page.slug}/
+    visit my_page.path
+    current_url.should match /#{my_page.slug}$/
     page.should have_selector 'h1', :text => my_page.title
     page.should have_selector 'title', :text => my_page.title + ': ' + Cooperative.configuration.application_name + ': ' + my_page.keywords
     page.should have_selector '[name="keywords"]', :content => my_page.keywords
