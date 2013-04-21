e8c505dff2daa29cf12a2f9e292a4d3c902cabf4
Karen Lundgren <karen.e.lundgren@gmail.com>
2013-04-21 16:48:42 -0600

    Attempting to correct error with attr_accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :nickname, :password, :password_confirmation, :remember_me, :public, :bio, :image
-  # attr_accessible :title, :body
   validates_presence_of :nickname
   
   has_many :messages, :foreign_key => :recipient_id
