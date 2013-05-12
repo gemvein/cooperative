394eb1f1928084cabc5442d28bf05ccffc75a0d9
Karen Lundgren <karen.e.lundgren@gmail.com>
2013-05-11 20:48:37 -0700

    Got statuses working as expected.

---
+++ app/models/status.rb
@@ -0,0 +1,6 @@
+class Status < ActiveRecord::Base
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
+  belongs_to :user
+  attr_accessible :body
+end
