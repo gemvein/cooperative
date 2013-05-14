cd487b4605830228e032d07e885a49021c007491
Karen Lundgren <karen.e.lundgren@gmail.com>
2013-05-14 08:02:57 -0700

    Switched existing features to requests, because features will come later.

--- spec/requests/people_spec.rb
+++ spec/requests/people_spec.rb
@@ -1,5 +1,39 @@
 require 'spec_helper'
 
 describe "People" do
-  
+  include_context "pages support"
+  include_context "login support for requests"
+  before(:each) do
+    FactoryGirl.create(:user, :public => true)
+    FactoryGirl.create(:user, :public => true)
+    FactoryGirl.create(:user, :public => true)
+    FactoryGirl.create(:user, :public => true)
+    FactoryGirl.create(:user, :public => true)
+    FactoryGirl.create(:user, :public => false, :nickname => 'private')
+  end
+
+  it "has a link in the navbar" do
+    user = FactoryGirl.create(:user)
+    sign_in(user)
+    visit cooperative.home_path
+    click_link :people.l
+  end
+
+  it "has a list of public people" do
+    user = FactoryGirl.create(:user)
+    sign_in(user)
+    visit cooperative.people_path
+    page.should have_selector '.person', :minimum => 5
+    page.should_not have_selector '.person', :text => 'private'
+  end
+
+  it "shows individual users" do
+    user = FactoryGirl.create(:user)
+    sign_in(user)
+    person = User.find_all_by_public(true).first
+    visit cooperative.person_path(person)
+    page.should have_selector 'h1', :text => person.nickname
+    page.should have_selector '.container', :exact => person.bio
+  end
+
 end
