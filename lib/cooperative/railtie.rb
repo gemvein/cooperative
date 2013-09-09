25f5b74f95381231d0abfb814f3be06c14624764
Karen Lundgren <karen.e.lundgren@gmail.com>
2013-09-09 16:30:10 -0600

    Cooperative now configuring Bootstrap Leather via a railtie.

--- lib/cooperative/railtie.rb
+++ lib/cooperative/railtie.rb
@@ -1,4 +1,12 @@
 module Cooperative
   class Railtie < Rails::Railtie
+    initializer 'cooperative.overwrite_bootstrap_leather_config', :after => :load_config_initializers do |app|
+      BootstrapLeather.configure do |config|
+        config.application_path = '/'
+        config.application_title = Cooperative.configuration.application_name
+        config.application_description = Cooperative.configuration.application_description
+        config.application_keywords = Cooperative.configuration.application_keywords
+      end
+    end
   end
 end
