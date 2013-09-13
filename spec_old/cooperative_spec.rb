f75749924fb7d0de6c7ebcb8d02dcce7c6ec4fff
Karen Lundgren <karen.e.lundgren@gmail.com>
2013-09-13 16:18:29 -0600

    Haven't referred to these once.  Getting rid of them.
    Can always use repository if I really need it.

--- spec_old/cooperative_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe "Cooperative" do
-  it 'should return correct version string' do
-    Cooperative.version_string.should == "Cooperative version #{Cooperative::VERSION}"
-  end
-end
